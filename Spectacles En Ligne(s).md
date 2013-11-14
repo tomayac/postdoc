18fe28a3c6caeac31fe683d2dc81276d364a2aa2
Thomas Steiner <steiner.thomas@gmail.com>
2013-11-14 15:51:42 +0100

    Initial commit.

---
+++ Spectacles En Ligne(s).md
@@ -0,0 +1,48 @@
+Le LIRIS a deux intérêts principaux dans le projet, liés d’une part à ses activités de construction de modèles de connaissances d'annotation dans des contextes applicatifs réels et au travail sur les modèles documentaires pour les Hypervidéos. D’autre part, ce projet sera également l'occasion de continuer à développer ses travaux dans le domaine de l’interaction homme-machine et de travailler sur les pratiques réflexives des troupes en répétition.
+
+TACHE 4 PROTOCOLE DOCUMENTAIRE (IRI-LIRIS)
+======================================
+
+4.1 Modèles documentaire et de connaissance
+===============================
+* **Objectifs:** Définir les modèles de connaissance, modèles documentaires nécessaires au projet.
+* **Responsable:** LIRIS
+* **Partenaires:** IRI, Ubicast, INRIA, Célestins (partenaire métier), Festival d’Aix (partenaire métier)
+* **Programme des travaux:**
+	* Interviews avec les experts métiers pour déterminer les types d’annotations adéquats à une description (i) d’une pièce de théâtre ou d’une pièce d’art lyrique, (ii) de la mise en scène associée, (iii) des répétitions, le tout en lien avec les éléments documentaires (texte de la pièce, documents de production, etc.).
+	* Définition de ces modèles sous la forme d’ontologies plus ou moins formelles, en réutilisant les modélisations existantes (par exemple TEI – Text Encoding Initiative – pour la description de documents).
+	* On étudiera en lien avec l’INRIA la possibilité dont certains de ces descripteurs pourraient faire l’objet d’une reconnaissance automatique, ou pourraient être annotées en direct ou a posteriori.
+	* Par ailleurs, on poursuivra les travaux sur la description documentaire à haut-niveau des hypervidéos (multiples vidéos, incrustation variées, sous-titrage, navigation, etc.), à partir d’annotations (Sadallah et al. 2011a, 2011b) pour la publication sur le Web, en lien avec les partenaires Ubicast et IRI qui les utiliseront dans leurs outils de génération et de construction d’hypervidéos (cf. tâche 5).
+* **Livrables:**
+	* L4.1.1: Modèles de connaissances pour les répétitions théâtrales (T0+8, T0+18 LIRIS)
+	* L4.1.2: Modèles documentaires pour les hypervidéos sur le Web (T0+8, T0+18 LIRIS)
+* **Risques et repli.:** Modèles trop complexes > simplification
+
+
+4.2 Corpus Linked Open Data (LIRIS)
+=========================
+* **Objectifs:** Être en capacité de publier les données du corpus sur le Web de données (en les liant si possible avec d'autres sources de données), dans la mouvance de l’initiative "Linked Open Data".
+* **Responsable:** LIRIS
+* **Partenaires:** IRI, UbiCast
+* **Programme des travaux:**
+	* Les données du corpus constitué doivent pouvoir être exposées sur le Web et être facilement exploitables/réutilisables. Dans ce contexte, nous vérifierons premièrement que les données issues de ces modèles (annotations) répondent aux exigences des données dites ouvertes et modifierons les modèles précédant si besoin est.
+	* Deuxièmement, nous nous assurerons de la conformité des modèles proposés aux technologies associées au Web de données (technologies du Web sémantique: RDF, etc.), fournirons une représentation de ces modèles à l’aide de ces technologies et les lierons dans la mesure du possible avec d’autres sources de données.
+	* Enfin, nous quantifierons les besoins en terme d’infrastructure (capacité serveur(s), etc.) nécessaires à la publication de ces données.
+* **Livrables:**
+	* L4.2.1: représentations des modèles de connaissance compatibles avec Open Data, liaison(s) éventuelle(s) avec d’autres sources de données (Linked Open Data) (T0+22 LIRIS)
+	*L4.2.2: étude sur l’infrastructure matérielle nécessaire à la publication des données (T0+22 LIRIS)
+* **Risques et repli.:** Aucun
+
+4.3 Maquettage d‘interactions (LIRIS)
+=========================
+* **Objectifs:** Définir les modes d’interaction innovants pour l’annotation.
+* **Responsable:** LIRIS
+* **Partenaires:** IRI, INRIA, UbiCast
+* **Programme des travaux:**
+	* Travail avec les utilisateurs, sketches, séances de créativité, maquettage et test des outils.
+	* Annotation par le geste, vocale, clavier/souris, par téléphone, par tablette, etc.
+	* On visera deux situations d’annotation: l’annotation temps réel pendant la répétition (le marquage) et l’annotation a posteriori soit par le metteur en scène, la troupe ou bien le responsable de la communication.
+* **Livrables:**
+	* L4.3.1: Interactions pour le marquage temps réel en répétition (T0+10 LIRIS)
+	* L4.3.2: Interactions pour l’annotation des bases de vidéos de répétition (T0+20 LIRIS)
+* **Risques et repli.:** Mauvaise prise en compte du métier > travail en collaboration étroite avec les producteurs de spectacles. Difficulté technique de réalisation > simplification des modes d’interaction.
---
+++ webvtt-demonstrator/webvtt.js
@@ -0,0 +1,167 @@
+(function() {
+
+  var videos = document.querySelectorAll('video');
+  for (var i = 0, lenI = videos.length; i < lenI; i++) {
+    var video = videos[i];
+    var textTracks = video.textTracks;
+    var metadataTextTracks = [];
+    for (var j = 0, lenJ = textTracks.length; j < lenJ; j++) {
+      var textTrack = textTracks[j];
+      console.log(textTrack);
+      if (textTrack.kind === 'metadata') {
+        metadataTextTracks.push(textTrack);
+        textTrack.mode = 'hidden';
+        textTrack.addEventListener('cuechange', function(e) {
+          oncuechange(e, video);
+        }, false);
+      }
+    }
+    video.addEventListener('canplaythrough', function() {
+      metadataTextTracks.forEach(function(textTrack) {
+        webVttToJsonLd(textTrack.cues, video.currentSrc);
+      });
+      video.play();
+    }, false);
+  }
+
+  var plugins = {
+    tags: function(video, activeCue, value, opt_speaker) {
+      var valueStringified = value.toString();
+      for (var i = 0, lenI = video.textTracks.length; i < lenI; i++) {
+        var textTrack = video.textTracks[i];
+        if (textTrack.mode === 'showing' || textTrack.default) {
+          for (var j = 0, lenJ = textTrack.activeCues.length; j < lenJ; j++) {
+            if (textTrack.activeCues[j].id === valueStringified) {
+              return;
+            }
+          }
+          var newCue = new TextTrackCue(
+              activeCue.startTime,
+              activeCue.endTime,
+              (opt_speaker ? opt_speaker : '<v.meta-tags>Scene Tags: ') +
+                  value.join(', '));
+          newCue.id = valueStringified;
+          textTrack.addCue(newCue);
+          break;
+        }
+      }
+    },
+    actors: function(video, activeCue, value) {
+      plugins.tags(video, activeCue, value,
+          '<v.meta-scene-actors>Scene Actors: ');
+    },
+    volume: function(video, activeCue, value) {
+      video.volume = parseFloat(value);
+    },
+    playbackRate: function(video, activeCue, value) {
+      video.playbackRate = parseFloat(value);
+    },
+    style: function(video, activeCue, value) {
+      var className = 'cue-' +
+          activeCue.startTime.toString().replace(/\./g, '_') + '-' +
+          activeCue.endTime.toString().replace(/\./g, '_');
+      if (!document.querySelector('#' + className)) {
+        var style = document.createElement('style');
+        style.id = className;
+        style.textContent = 'video.' + className + ' {' + value + ';}';
+        style.setAttribute('type', 'text/css');
+        document.head.appendChild(style);
+        video.classList.add(className);
+        (function(currentClassName) {
+          activeCue.addEventListener('exit', function(e) {
+            var classList = video.classList;
+            classList.remove(className);
+            currentTime = parseFloat(video.currentTime);
+            try {
+              document.querySelector('#' + className).remove();
+              // not all cue.onexit events always fire, so clean up manually
+              for (var i = 0, lenI = classList.length; i < lenI; i++) {
+                var currentClassName = classList.item(i);
+                var cueEndTime = parseFloat(currentClassName.split(/-/g)[2]
+                    .replace(/_/g, '.'));
+                if (currentTime > cueEndTime) {
+                  classList.remove(currentClassName);
+                  document.querySelector('#' + currentClassName).remove();
+                }
+              }
+            } catch(e) {
+              // no-op
+            }
+          }, false);
+        })(className);
+      }
+    },
+    spatialFragment: function(video, activeCue, value) {
+      var components = value.replace(/^xywh=/, '').split(/,/);
+      var x = components[0];
+      var y = components[1];
+      var width = components[2];
+      var height = components[3];
+      var div = document.createElement('div');
+      var id = 'cue-' +
+          activeCue.startTime.toString().replace(/\./g, '_') + '-' +
+          activeCue.endTime.toString().replace(/\./g, '_');
+      div.id = id;
+      div.style.border = 'solid 4px yellow';
+      video.parentNode.appendChild(div);
+      div.style.position = 'absolute';
+      div.style.left = (video.offsetLeft + parseInt(x, 10)) + 'px';
+      div.style.top = (video.offsetTop + parseInt(y, 10)) + 'px';
+      div.style.width = width + 'px';
+      div.style.height = height + 'px';
+      div.style.zIndex = 10000;
+      (function(currentId) {
+        activeCue.addEventListener('exit', function(e) {
+          try {
+            document.querySelector('#' + currentId).remove();
+          } catch(e) {
+            // no-op
+          }
+        }, false);
+      })(id);
+    }
+  };
+
+  function oncuechange(e, video) {
+    var activeCues = e.target.activeCues;
+    for (var i = 0, len = activeCues.length; i < len; i++) {
+      var activeCue = activeCues[i];
+      if (!activeCue) {
+        return;
+      }
+      var data = JSON.parse(activeCue.text);
+      for (var key in data) {
+        if (plugins[key]) {
+          plugins[key](video, activeCue, data[key]);
+        }
+      }
+    }
+  }
+
+  function webVttToJsonLd(cues, src) {
+    var jsonLd = {
+      "@context": {
+        annotation: 'http://example.org/annotation',
+        hasFragment: 'http://www.w3.org/ns/ma-ont#hasFragment'
+      },
+      "@id": src,
+      "@type": 'http://www.w3.org/ns/ma-ont#MediaResource',
+      hasFragment: []
+    };
+    for (var i = 0, lenI = cues.length; i < lenI; i++) {
+      var cue = cues[i];
+      var mediaFragment = {};
+      jsonLd.hasFragment.push(mediaFragment);
+      mediaFragment['@id'] = src + '#t=' + cue.startTime + ',' + cue.endTime;
+      mediaFragment['@type'] = 'http://www.w3.org/ns/ma-ont#MediaFragment';
+      mediaFragment.annotations = JSON.parse(cue.text);
+    }
+    var fragment = document.createDocumentFragment();
+    var pre = document.createElement('pre');
+    pre.innerHTML = JSON.stringify(jsonLd, null, 2);
+    fragment.appendChild(pre);
+    document.body.appendChild(fragment);
+    return JSON.stringify(jsonLd, null, 2);
+  }
+
+})();
