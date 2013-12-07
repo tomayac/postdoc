b1f0dcc36aa63457d533eaf5560f417c92ed10d1
Ruben Verborgh <ruben@verborgh.org>
2013-12-07 16:31:44 +0000

    Display active cues.

--- demos/webvtt-demonstrator/CueEditor.js
+++ demos/webvtt-demonstrator/CueEditor.js
@@ -3,35 +3,51 @@
   var forEach = Function.prototype.call.bind([].forEach);
 
   function CueEditor(video) {
-    var start = $('#start'),
+    var self = this,
+        start = $('#start'),
         end = $('#end'),
-        cueList = $('#activeCues ul');
+        cueList = this._cueList = $('#activeCues ul');
 
+    // cue list
+    video.metadataTrack.addEventListener('cuechange', function () {
+      self._displayCues(this.activeCues);
+    });
+
+    // start and end controls
     video.addEventListener('loadedmetadata', function () {
       start.value = start.min = end.min = 0;
       end.value   = start.max = end.max = Math.floor(video.duration);
     });
-
-    video.metadataTrack.addEventListener('cuechange', function () {
-      cueList.innerHTML = '';
-      forEach(this.activeCues, function (cue) {
-        var li = document.createElement('li');
-        li.innerText = cue.id;
-        cueList.appendChild(li);
-      });
-    });
-
     start.addEventListener('change', function () {
       if (parseFloat(end.value) < parseFloat(start.value))
         end.value = start.value;
       video.currentTime = start.value;
     });
-
     end.addEventListener('change', function () {
       if (parseFloat(start.value) > parseFloat(end.value))
         start.value = end.value;
       video.currentTime = end.value;
     });
+  }
+
+  CueEditor.prototype = {
+    _displayCues: function (cues) {
+      var cueList = this._cueList;
+      cueList.innerHTML = '';
+      forEach(cues, function (cue) {
+        var cueElement = addChild(cueList, 'li'),
+            cueLink = addChild(cueElement, 'a', cue.id);
+        cueLink.href = 'javascript:';
+      });
+    },
+  };
+
+  function addChild(parent, tagName, text) {
+    var element = document.createElement(tagName);
+    parent.appendChild(element);
+    if (text)
+      element.innerText = text;
+    return element;
   }
 
   root.CueEditor = CueEditor;
