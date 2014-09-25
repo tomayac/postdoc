4055360755798767b64440105cb9ac9b892e4554
tomayac <steiner.thomas@gmail.com>
2014-09-25 09:42:03 +0200

    Added search widget.

--- demos/polymer-hypervideo/static/polymer-hypervideo/elements/polymer-track-subtitles.js
+++ demos/polymer-hypervideo/static/polymer-hypervideo/elements/polymer-track-subtitles.js
@@ -6,10 +6,14 @@ Polymer('polymer-track-subtitles', {
   ready: function() {
     var that = this;
     var container = that.$.container;
+    var search = that.$.search;
+    var cuesContainer = that.$.cuesContainer;
     if (that.width) {
       container.style.width = that.width + 'px';
+      search.style.width = 'calc(' + that.width + 'px - 2em)';
     } else {
       container.style.width = '50%';
+      search.style.width = '50%';
     }
     if (that.height) {
       container.style.height = that.height + 'px';
@@ -34,26 +38,52 @@ Polymer('polymer-track-subtitles', {
     });
 
     var displaySubtitlesGroup = function(cues) {
+      // Full-text search
+      search.style.display = 'block';
+      var doSearch = function() {
+        var query = search.value;
+        var regExp;
+        try {
+          regExp = new RegExp(query, 'gi');
+          var divs = cuesContainer.querySelectorAll('div');
+          for (var i = 0, lenI = divs.length; i < lenI; i++) {
+            var div = divs[i];
+            div.style.backgroundColor = 'transparent';
+            regExp.lastIndex = 0;
+            var matches = div.textContent.match(regExp);
+            if (matches && query.length) {
+              matches.forEach(function(match) {
+                div.style.backgroundColor = 'yellow';
+              });
+            }
+          }
+        } catch(e) {
+          console.log('Invalid search term: ' + e);
+        }
+      };
+      search.addEventListener('keyup', doSearch, false);
+      search.addEventListener('change', doSearch, false);
+      search.addEventListener('search', doSearch, false);
+      // Attach cues
       var tempDiv = document.createElement('div');
       cues.forEach(function(cue) {
-        var span = document.createElement('span');
-        span.dataset.start = cue.start;
-        span.dataset.end = cue.end;
+        var div = document.createElement('div');
+        div.dataset.start = cue.start;
+        div.dataset.end = cue.end;
         tempDiv.innerHTML = cue.text;
-        span.innerHTML = tempDiv.textContent;
-        container.appendChild(span);
-        container.appendChild(document.createElement('br'));
-        cuesElements.push(span);
+        div.innerHTML = tempDiv.textContent;
+        cuesContainer.appendChild(div);
+        cuesElements.push(div);
       });
     };
 
-    container.addEventListener('click', function(e) {
-      console.log('Received event (container): click');
+    cuesContainer.addEventListener('click', function(e) {
+      console.log('Received event (cuesContainer): click');
       var current = e.target;
-      if (current === container) {
+      if (current === cuesContainer) {
         return;
       }
-      while (current.nodeName !== 'SPAN') {
+      while (current.nodeName !== 'DIV') {
         current = current.parentNode;
       }
       // console.log('Fired event: currenttimeupdate');
