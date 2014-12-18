6d53217370878a7dda00d432365780869d9552f5
tomayac <steiner.thomas@gmail.com>
2014-12-18 15:55:15 +0100

    Some changes for Heroku.

--- demos/polymer-hypervideo/app.js
+++ demos/polymer-hypervideo/app.js
@@ -5,15 +5,16 @@ var app = express();
 var http = require('http');
 var server = http.createServer(app);
 
-app.use('/webcomponentsjs', express.static(__dirname + '/webcomponentsjs'));
-app.use('/webvtt-parser', express.static(__dirname + '/webvtt-parser'));
-app.use('/polymer-ldf-client', express.static(__dirname + '/polymer-ldf-client'));
-app.use('/polymer-hypervideo', express.static(__dirname + '/polymer-hypervideo'));
-app.use('/core-component-page', express.static(__dirname + '/core-component-page'));
-app.use('/media-fragments-uri', express.static(__dirname + '/media-fragments-uri'));
-app.use('/polymer', express.static(__dirname + '/polymer'));
-app.use('/spectacle-en-lignes', express.static(__dirname + '/spectacle-en-lignes'));
-app.use('/', express.static(__dirname + '/spectacle-en-lignes'));
+process.env.PWD = process.cwd();
+app.use('/webcomponentsjs', express.static(process.env.PWD + '/webcomponentsjs'));
+app.use('/webvtt-parser', express.static(process.env.PWD + '/webvtt-parser'));
+app.use('/polymer-ldf-client', express.static(process.env.PWD + '/polymer-ldf-client'));
+app.use('/polymer-hypervideo', express.static(process.env.PWD + '/polymer-hypervideo'));
+app.use('/core-component-page', express.static(process.env.PWD + '/core-component-page'));
+app.use('/media-fragments-uri', express.static(process.env.PWD + '/media-fragments-uri'));
+app.use('/polymer', express.static(process.env.PWD + '/polymer'));
+app.use('/spectacle-en-lignes', express.static(process.env.PWD + '/spectacle-en-lignes'));
+app.use('/', express.static(process.env.PWD + '/spectacle-en-lignes'));
 
 // start the server
 var port = process.env.PORT || 5000;
