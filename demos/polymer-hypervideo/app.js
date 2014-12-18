3c8a6b8848a3df3f750f6c3ade92818d8a33fdbf
tomayac <steiner.thomas@gmail.com>
2014-12-18 15:36:24 +0100

    Added favicon. More specific routes.

--- demos/polymer-hypervideo/app.js
+++ demos/polymer-hypervideo/app.js
@@ -5,11 +5,15 @@ var app = express();
 var http = require('http');
 var server = http.createServer(app);
 
-app.use(express.static(__dirname + '/'));
-
-app.get('/spectacle-en-lignes', function(req, res) {
-  res.sendfile(__dirname + '/spectacle-en-lignes/index.html');
-});
+app.use('/webcomponentsjs', express.static(__dirname + '/webcomponentsjs'));
+app.use('/webvtt-parser', express.static(__dirname + '/webvtt-parser'));
+app.use('/polymer-ldf-client', express.static(__dirname + '/polymer-ldf-client'));
+app.use('/polymer-hypervideo', express.static(__dirname + '/polymer-hypervideo'));
+app.use('/core-component-page', express.static(__dirname + '/core-component-page'));
+app.use('/media-fragments-uri', express.static(__dirname + '/media-fragments-uri'));
+app.use('/polymer', express.static(__dirname + '/polymer'));
+app.use('/spectacle-en-lignes', express.static(__dirname + '/spectacle-en-lignes'));
+app.use('/', express.static(__dirname + '/spectacle-en-lignes'));
 
 // start the server
 var port = process.env.PORT || 5000;
